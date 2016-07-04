d30c5d7b6ce4a36b90fc54e8321e55d072c6f698
petrero <petrero@gmail.com>
2016-07-04 21:39:25 +0300

    Door -count properties is a static property. Being attached to the class itself and not to instances it's incremented at each instantiation of Car class.

--- constructor_destructor.php
+++ constructor_destructor.php
@@ -1,12 +1,15 @@
 <?php
 class Cars {
   public $wheel_count = 4;
-  
+  static  $door_count = 4;
           
   function __construct(){
-    echo $this->wheel_count;
+    //echo $this->wheel_count;
+    echo self::$door_count++;
   }
   
 }
 
 $bmw = new Cars();
+$mercedes = new Cars();
+$mercedes2 = new Cars();
