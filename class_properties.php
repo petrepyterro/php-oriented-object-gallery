e127727b63f33b204f788a589fcff766e967b03f
petrero <petrero@gmail.com>
2016-07-04 19:57:25 +0300

    Create some class properties. Change the value of one of them outside the class and reference one of them in the method class.

---
+++ class_properties.php
@@ -0,0 +1,19 @@
+<?php
+class Cars {
+  var $wheel_count = 4;
+  var $door_count = 4;
+  
+  function car_detail(){
+    echo "This car has " . $this->wheel_count . " wheels";
+  }
+  
+}
+
+$bmw = new Cars();
+$mercedes = new Cars();
+
+echo $bmw->wheel_count=10;
+echo "<br/>";
+echo $mercedes->wheel_count . "<br/>";
+
+echo $mercedes->car_detail();
