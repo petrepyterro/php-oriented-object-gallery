3efd5ded3cbb8e70d5006f65644a5b35803b40c3
petrero <petrero@gmail.com>
2016-07-04 20:31:17 +0300

    Inside static methods class we need to use static properties.Static properties are assigned to the class. Regular values either publi, private or protected are assigned to the instance. We don't need to instantiate a class to access static methods

---
+++ static_modifier.php
@@ -0,0 +1,14 @@
+<?php
+class Cars {
+  static $wheel_count = 4;
+  static $door_count = 4;
+          
+  static function car_detail(){
+    echo Cars::$wheel_count;
+    echo Cars::$door_count;
+  }
+  
+}
+
+echo Cars::$door_count . "<br/>";
+Cars::car_detail();
