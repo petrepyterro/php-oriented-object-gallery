b26a1e39d1fa3c538894ad267fc2de9b823fc10a
petrero <petrero@gmail.com>
2016-07-05 21:57:53 +0300

    Assigning array values to object properties.

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,11 +1,17 @@
 <?php
 
 class User {
+  public $id;
+  public $password;
+  public $username;
+  public $firstname;
+  public $lastname;
+  
   public static function find_all_users(){
     self::find_this_query("SELECT * FROM users");
   }
   
-  public function find_user_by_id($id){
+  public static function find_user_by_id($id){
     
     $result_set = self::find_this_query("SELECT * FROM users WHERE user_id=$id LIMIT 1");
     $found_user = mysqli_fetch_array($result_set);
