1ee92e431abc0849114b1cc69b19a7e9528cd019
petrero <petrero@gmail.com>
2016-07-05 19:44:44 +0300

    Improve our database connection to be more object oriented. Connection variable of the database class is now a class by itself so later we can call the methods of this class.

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,12 +7,7 @@
         Admin
         <small>Subheading</small>
       </h1>
-      <?php 
-        $sql = "SELECT * FROM users";
-        $result = $database->query($sql);
-        $user_found = mysqli_fetch_array($result);
-        echo $user_found['username'];
-      ?>
+      
       <ol class="breadcrumb">
         <li>
           <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
--- admin/includes/database.php
+++ admin/includes/database.php
@@ -9,28 +9,33 @@ class Database {
   }
 
   public function open_db_connection() {
-    $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    //$this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     
-    if(mysqli_connect_error()){
-      die("Database connection failed badly " . mysqli_error());
+    if($this->connection->connect_errno){
+      die("Database connection failed badly " . $this->connection->connect_error);
     }
   }
   
   public function query($sql){
-    $result = mysqli_query($this->connection, $sql);
+    $result = $this->connection->query($sql);
     $this->confirmQuery($result);
     return $result;
   }
   
   private function confirmQuery($result){
     if (!$result){
-      die("Query failed " . mysqli_error($this->connection));
+      die("Query failed " . $this->connection->error);
     }
   }
   
   public function escape_string($string){
-    $escaped_string = mysqli_real_escape_string($this->connection, $string);
+    $escaped_string = $this->connection->real_escape_string($string);
     return $escaped_string;
+  }
+  
+  public function the_insert_id(){
+    return $this->connection->insert_id;
   }
   
 }
