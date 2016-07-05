f634c3175541570ec703c9ef78f3d1e1a128dec7
petrero <petrero@gmail.com>
2016-07-05 19:15:18 +0300

    Test our query method.

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,6 +7,12 @@
         Admin
         <small>Subheading</small>
       </h1>
+      <?php 
+        $sql = "SELECT * FROM users";
+        $result = $database->query($sql);
+        $user_found = mysqli_fetch_array($result);
+        echo $user_found['username'];
+      ?>
       <ol class="breadcrumb">
         <li>
           <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
