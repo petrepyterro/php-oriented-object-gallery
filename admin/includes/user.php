c52ac02afbce2882d705a72e1126e7d0e678d173
petrero <petrero@gmail.com>
2016-07-05 23:08:15 +0300

    Short way autoinstantiation.

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -25,16 +25,27 @@ class User {
     return $result_set;
   }
   
-  public static function instantiation($found_user){
+  public static function instantiation($the_record){
     $the_object = new self();
     
-    $the_object->id = $found_user['user_id'];
-    $the_object->username = $found_user['username'];
-    $the_object->password = $found_user['user_password'];
-    $the_object->firstname = $found_user['user_firstname'];
-    $the_object->lastname = $found_user['user_lastname'];
-    
+    //$the_object->id = $found_user['user_id'];
+    //$the_object->username = $found_user['username'];
+    //$the_object->password = $found_user['user_password'];
+    //$the_object->firstname = $found_user['user_firstname'];
+    //$the_object->lastname = $found_user['user_lastname'];
+    foreach($the_record as $the_attribute => $value){
+      if($the_object->has_the_attribute($the_attribute)){
+        $the_object->the_attribute = $value;
+      }
+    }
     return $the_object;
   }
+  
+  private function has_the_attribute($the_attribute){
+    $object_properties = get_object_vars($this);
+    return array_key_exists($the_attribute, $object_properties);
+  }
 }
+
+
 
