31226a3d252dbe8bf723009d7a29e5350ec171f6
petrero <petrero@gmail.com>
2016-07-05 18:30:21 +0300

    Create the database class. My database connection will be established in every page automatically.

---
+++ admin/includes/database.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'new_config.php';
+
+class Database {
+  public $connection;
+  
+  public function __construct() {
+    $this->open_db_connection();
+  }
+
+  public function open_db_connection() {
+    $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    
+    if(mysqli_connect_error()){
+      die("Database connection failed badly " . mysqli_error());
+    }
+  }
+  
+}
+
+$database = new Database();
