3de151e14019b46693195d1d4fcb73afacd0be29
petrero <petrero@gmail.com>
2016-07-06 20:29:59 +0300

    Make some final adjusments to autoload function registered with spl_autoload_register.

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -4,11 +4,9 @@ function classAutoLoader($class){
   $class = strtolower($class);
   $the_path = "includes/{$class}.php";
   
-  if(file_exists($the_path)){
-    require_once($the_path);
-  } else {
-    die("This file name {$class}.php was not found");
-  }
+  if(is_file($the_path)){
+    include($the_path);
+  } 
 }
 
 spl_autoload_register("classAutoLoader");
