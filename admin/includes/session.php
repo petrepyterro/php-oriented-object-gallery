45fa1f4b1f38dd7d9a62551f87619645ac00847d
petrero <petrero@gmail.com>
2016-07-06 21:08:46 +0300

    Create a function that checks if someone is logged in, a getter function that gets signed_in private property and a login function that login the user if exits in the database.

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,10 +1,34 @@
 <?php
 
 class Session {
-  private $signed_in;
+  private $signed_in = FALSE;
   public $user_id;
           
   function __construct() {
     session_start();
+    $this->check_the_login();
+  }
+  
+  public function is_signed_in(){
+    return $this->signed_in;
+  }
+  
+  public function login($user){
+    if($user){
+      $this->user_id = $_SESSION['user_id'] = $user->id;
+      $this->signed_in = TRUE;
+    }
+  }
+
+  private function check_the_login(){
+    if(isset($_SESSION['user_id'])){
+      $this->user_id = $_SESSION['user_id'];
+      $this->signed_in = TRUE;
+    } else {
+      unset($this->user_id);
+      $this->signed_in = FALSE;
+    }
   }
 }
+
+$session = new Session();
