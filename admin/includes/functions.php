183cc07b3214d88803f93e69f2bbd8e5b96f717b
petrero <petrero@gmail.com>
2016-07-06 21:38:51 +0300

    Create login page and redirect the user to login page if he tries to access admin area without log in.

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -10,3 +10,7 @@ function classAutoLoader($class){
 }
 
 spl_autoload_register("classAutoLoader");
+
+function redirect($location){
+  header("Location: $location");
+}
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,5 +1,11 @@
 <?php ob_start(); ?>
 <?php require_once "init.php"; ?>
+
+<?php 
+  if(!$session->is_signed_in()){
+    redirect("login.php");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
