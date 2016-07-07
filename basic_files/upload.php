392f1c798708b18945e1779791c484d37b0c32c3
petrero <petrero@gmail.com>
2016-07-07 18:58:49 +0300

    Display file upload error code messages.

--- basic_files/upload.php
+++ basic_files/upload.php
@@ -1,7 +1,25 @@
 <?php 
-  echo "<pre>";
-  print_r($_FILES['file_upload']);
-  echo "</pre>";
+  if(isset($_POST['submit'])){
+    echo "<pre>";
+    print_r($_FILES['file_upload']);
+    echo "</pre>";
+  
+  
+    $upload_errors = array(
+
+      UPLOAD_ERR_OK             => "There is no error",
+      UPLOAD_ERR_INI_SIZE       => "The uploaded file exceeds the upload_max_filesize directive",
+      UPLOAD_ERR_FORM_SIZE      => "The uploaded file exceeds the MAX_FILE_SIZE directive",
+      UPLOAD_ERR_PARTIAL        => "The uploaded file was only partially uploaded",
+      UPLOAD_ERR_NO_FILE        => "No file was uploaded",
+      UPLOAD_ERR_NO_TMP_DIR     => "Missing a temporary folder",
+      UPLOAD_ERR_CANT_WRITE     => "Failed to write file to disk",
+      UPLOAD_ERR_EXTENSION      => "A PHP extension stopped the file upload"  
+    );
+    
+    $the_error = $_FILES['file_upload']['error'];
+    $the_message = $upload_errors[$the_error];
+  }       
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,6 +30,13 @@
   </head>
   <body>
     <form action="upload.php" method="POST" enctype="multipart/form-data">
+      <h2>
+        <?php 
+          if(!empty($upload_errors)){
+            echo $the_message;
+          }
+        ?>
+      </h2>
       <input type="file" name="file_upload"/><br>
       <input type="submit" name="submit"/>
     </form>
