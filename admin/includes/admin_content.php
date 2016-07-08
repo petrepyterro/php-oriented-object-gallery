96a0436d5b70e614feeeb7a50a0af3be6438e0db
petrero <petrero@gmail.com>
2016-07-08 17:27:37 +0300

    Copy code from previous branch.

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,26 @@
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">
+        Admin
+        <small>Subheading</small>
+      </h1>
+      <?php 
+        $user = User::find_user_by_id(3);
+        $user->delete();
+      ?>
+      <ol class="breadcrumb">
+        <li>
+          <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+        </li>
+        <li class="active">
+          <i class="fa fa-file"></i> Blank Page
+        </li>
+      </ol>
+    </div>
+  </div>
+  <!-- /.row -->
+
+</div>
