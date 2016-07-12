9fb70d2444a6150ec0188d09ac34e53e97db4d97
petrero <petrero@gmail.com>
2016-07-12 21:08:11 +0300

    Add WYSIWYG editor to our edit photo page.

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -6,7 +6,11 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
+    
+    <!-- WYSIWYG editor -->
+    <script src="http://tinymce.cachefly.net/4.1/tinymce.min.js"></script>
 
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
