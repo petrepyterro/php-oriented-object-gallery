0fad2b1a319eb9a233068d41cd4896f754a8000a
petrero <petrero@gmail.com>
2016-07-12 21:23:07 +0300

    Copy code from previous code.

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,50 @@
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <div class="row">
+    <div class="col-lg-12">
+      <h1 class="page-header">
+        Admin
+        <small>Subheading</small>
+      </h1>
+      <?php 
+        /*
+        $photos = Photo::find_all();
+        foreach($photos as $photo){
+          echo $photo->photo_title;
+        }
+         * 
+         */
+        
+        /*
+        $photo = new Photo();
+        $photo->photo_title = "Belgium";
+        $photo->photo_size = 20;
+        
+        $photo->save();
+         * 
+         */
+      
+        /*
+        $photo = Photo::find_by_id(1);
+        $photo->photo_filename = "large_image.jpg";
+        
+        $photo->save();
+        * 
+        */
+      
+        
+      ?>
+      <ol class="breadcrumb">
+        <li>
+          <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+        </li>
+        <li class="active">
+          <i class="fa fa-file"></i> Blank Page
+        </li>
+      </ol>
+    </div>
+  </div>
+  <!-- /.row -->
+
+</div>
---
+++ admin/includes/user.php
@@ -0,0 +1,33 @@
+<?php
+
+class User extends Db_object{
+  
+  protected static $db_table = "users";
+  protected static $db_table_fields = array("username", "user_password", "user_firstname", "user_lastname");
+  public $id;
+  public $user_password;
+  public $username;
+  public $user_firstname;
+  public $user_lastname;
+  
+  
+  
+  public static function verify_user($username, $password){
+    global $database;
+    
+    $username = $database->escape_string($username);
+    $password = $database->escape_string($password);
+    
+    $sql = "SELECT * FROM " . self::$db_table . " WHERE username='$username' AND user_password='$password' LIMIT 1";
+    $the_result_array = self::find_by_query($sql);
+    return !empty($the_result_array) ? array_shift($the_result_array) : false; 
+  }
+
+  
+  
+  
+  
+}
+
+
+
