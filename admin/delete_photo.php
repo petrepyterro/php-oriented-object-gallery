1ab8c46c1bcf61df96133d226525b2be92166b23
petrero <petrero@gmail.com>
2016-07-12 17:23:52 +0300

    Code delete_photo page.

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -4,4 +4,14 @@
     redirect("login.php");
   }
   
-  echo("it works");
+if(empty($_GET['id'])){
+  redirect("photos.php");
+}
+
+$photo = Photo::find_by_id($_GET['id']);
+
+if($photo){
+  $photo->delete_photo();
+} else {
+  redirect("photos.php");
+}
