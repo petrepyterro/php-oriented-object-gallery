77a035e8a300b6406f7e32fed02621219f132e40
petrero <petrero@gmail.com>
2016-07-12 22:57:47 +0300

    Create form for adding users.

--- admin/users.php
+++ admin/users.php
@@ -23,8 +23,8 @@
         <div class="col-lg-12">
           <h1 class="page-header">
            Users
-            <small>Subheading</small>
           </h1>
+          <a href="add_user.php" class="btn btn-primary">Add User</a>
           <div class="col-md-12">
             <table class="table table-hover">
               <thead>
