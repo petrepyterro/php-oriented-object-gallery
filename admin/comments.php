afae863c6c781abfce4dde050a03b5f4c9a9ab12
petrero <petrero@gmail.com>
2016-07-14 21:11:08 +0300

    Displaying comments on amin back end side.

--- admin/comments.php
+++ admin/comments.php
@@ -1,4 +1,6 @@
 <?php include("includes/header.php"); ?>
+<?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
+<?php $comments = Comment::find_all(); ?>
 
   <!-- Navigation -->
   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -21,16 +23,32 @@
         <div class="col-lg-12">
           <h1 class="page-header">
            Comments
-            <small>Subheading</small>
           </h1>
-          <ol class="breadcrumb">
-            <li>
-              <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-            </li>
-            <li class="active">
-              <i class="fa fa-file"></i> Blank Page
-            </li>
-          </ol>
+          <div class="col-md-12">
+            <table class="table table-hover">
+              <thead>
+                <tr >
+                  <th>Id</th>
+                  <th>Author</th>
+                  <th>Body</th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php foreach($comments as $comment) : ?>
+                <tr>
+                  <td><?php echo $comment->id; ?></td>
+                  <td>
+                    <?php echo $comment->author; ?>
+                    <div class="action_links">
+                      <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>
+                    </div>
+                  </td>
+                  <td><?php echo $comment->body; ?></td>
+                </tr>
+              <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
       <!-- /.row -->
