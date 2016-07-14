7ae5e607c744262cd2f53b0dcad4440c0bedd72f
petrero <petrero@gmail.com>
2016-07-14 18:34:07 +0300

    Self instantiation Comment method.

---
+++ admin/includes/comment.php
@@ -0,0 +1,45 @@
+<?php
+
+class Comment extends Db_object{
+  
+  protected static $db_table = "users";
+  protected static $db_table_fields = array("id", "photo_id", "author", "body");
+  public $id;
+  public $photo_id;
+  public $author;
+  public $body;
+  
+  public $tmp_path;
+  public $user_image;
+  public $upload_directory = "images" . DS . "users";
+  public $image_placeholder = "http://placehold.it/400x400&text=image";
+  public $errors;
+  public $upload_errors_array = array(
+    UPLOAD_ERR_OK             => "There is no error",
+    UPLOAD_ERR_INI_SIZE       => "The uploaded file exceeds the upload_max_filesize directive",
+    UPLOAD_ERR_FORM_SIZE      => "The uploaded file exceeds the MAX_FILE_SIZE directive",
+    UPLOAD_ERR_PARTIAL        => "The uploaded file was only partially uploaded",
+    UPLOAD_ERR_NO_FILE        => "No file was uploaded",
+    UPLOAD_ERR_NO_TMP_DIR     => "Missing a temporary folder",
+    UPLOAD_ERR_CANT_WRITE     => "Failed to write file to disk",
+    UPLOAD_ERR_EXTENSION      => "A PHP extension stopped the file upload"   
+  );
+  
+  
+  public static function create_comment($photo_id, $author="John", $body=""){
+    if(!empty($photo_id) && !empty($author) && !empty($body)){
+      $comment = new Comment();
+      $comment->photo_id  = (int)$photo_id;
+      $comment->author    = $author;
+      $comment->body      = $body;
+      
+      return $comment;
+    } else {
+      return FALSE;
+    }
+  }
+  
+ }
+
+
+
