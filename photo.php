21811368a69279aab206988f894e5bd210139ce6
petrero <petrero@gmail.com>
2016-07-14 19:42:43 +0300

    Include our classes in photo.php

--- photo.php
+++ photo.php
@@ -1,3 +1,17 @@
+<?php 
+  require_once("admin/includes/init.php");
+  
+  if(empty($_GET['id'])){
+    redirect("index.php");
+  }
+  
+  $photo = Photo::find_by_id($_GET['id']);
+  echo $photo->photo_title;
+  
+  if (isset($_POST['submit'])){
+    echo "Hello";
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -106,9 +120,13 @@
             <h4>Leave a Comment:</h4>
             <form role="form">
               <div class="form-group">
-                <textarea class="form-control" rows="3"></textarea>
+                <label for="author">Author</label>
+                <input type="text" name="author" class="form-control"/>
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <div class="form-group">
+                <textarea name="body" class="form-control" rows="3"></textarea>
+              </div>
+              <button type="submit" name="submit" class="btn btn-primary">Submit</button>
             </form>
           </div>
 
@@ -139,19 +157,6 @@
                   <small>August 25, 2014 at 9:30 PM</small>
                 </h4>
                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                <!-- Nested Comment -->
-                <div class="media">
-                  <a class="pull-left" href="#">
-                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                  </a>
-                  <div class="media-body">
-                    <h4 class="media-heading">Nested Start Bootstrap
-                      <small>August 25, 2014 at 9:30 PM</small>
-                    </h4>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                  </div>
-                </div>
-                <!-- End Nested Comment -->
               </div>
             </div>
 
