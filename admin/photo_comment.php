069f6230d1b716e8d85b29266dbf8c5d67216a07
petrero <petrero@gmail.com>
2016-07-14 21:44:12 +0300

    Create individual comments page for a photo on admin back end.

--- admin/photo_comment.php
+++ admin/photo_comment.php
@@ -1,6 +1,13 @@
 <?php include("includes/header.php"); ?>
 <?php if(!$session->is_signed_in()) {redirect("login.php");} ?>
-<?php $photos = Photo::find_all(); ?>
+<?php 
+  if(empty($_GET['id'])){
+    redirect("photos.php");
+  }
+  
+  $comments = Comment::find_the_comments($_GET['id']);
+  $photo = Photo::find_by_id($_GET['id']);
+?>
 
   <!-- Navigation -->
   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -22,35 +29,29 @@
       <div class="row">
         <div class="col-lg-12">
           <h1 class="page-header">
-           Photos
-            <small>Subheading</small>
+            <p>Comments for photo</p>
+            <img class="admin-photo-thumbnail" src="<?php echo $photo->picture_path(); ?>" alt=""/>
           </h1>
           <div class="col-md-12">
             <table class="table table-hover">
               <thead>
-                <tr >
-                  <th>Photo</th>
+                <tr>
                   <th>Id</th>
-                  <th>File Name</th>
-                  <th>Title</th>
-                  <th>Size</th>
+                  <th>Author</th>
+                  <th>Body</th>
                 </tr>
               </thead>
               <tbody>
-              <?php foreach($photos as $photo) : ?>
+              <?php foreach($comments as $comment) : ?>
                 <tr>
+                  <td><?php echo $comment->id; ?></td>
                   <td>
-                    <img class="admin-photo-thumbnail" src="<?php echo $photo->picture_path(); ?>" alt=""/>
-                    <div class="picture_link">
-                      <a href="delete_photo.php?id=<?php echo $photo->id; ?>">Delete</a>
-                      <a href="edit_photo.php?id=<?php echo $photo->id; ?>">Edit</a>
-                      <a href="../photo.php?id=<?php echo $photo->id; ?>">View</a>
+                    <?php echo $comment->author; ?>
+                    <div class="action_links">
+                      <a href="delete_comment.php?id=<?php echo $comment->id; ?>">Delete</a>
                     </div>
                   </td>
-                  <td><?php echo $photo->id; ?></td>
-                  <td><?php echo $photo->photo_filename; ?></td>
-                  <td><?php echo $photo->photo_title; ?></td>
-                  <td><?php echo $photo->photo_size; ?></td>
+                  <td><?php echo $comment->body; ?></td>
                 </tr>
               <?php endforeach; ?>
               </tbody>
