82764f9f2bdd2f6fb766a90e0c88731e9631278a
petrero <petrero@gmail.com>
2016-07-15 20:14:12 +0300

    Add Google API chart pie variant.

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -109,6 +109,10 @@
 
       </div> <!--First Row-->
       
+      <div class="row">
+        <div id="piechart" style="width: 900px; height: 500px;"></div>
+      </div>
+      
     </div>
   </div>
   <!-- /.row -->
--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -11,6 +11,30 @@
     <script src="http://tinymce.cachefly.net/4.1/tinymce.min.js"></script>
 
     <script src="js/scripts.js"></script>
+    
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Language', 'Speakers (in millions)'],
+          ['German',  5.85],
+          ['French',  1.66],
+          ['Italian', 0.316],
+          ['Romansh', 0.0791]
+        ]);
+
+      var options = {
+        legend: 'none',
+        pieSliceText: 'label',
+        title: 'Swiss Language Use (100 degree rotation)',
+        pieStartAngle: 100,
+      };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+    </script>
 </body>
 
 </html>
