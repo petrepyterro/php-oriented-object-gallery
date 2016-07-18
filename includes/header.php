231a6c4254b1a95107c564dbbf3869606785828b
petrero <petrero@gmail.com>
2016-07-18 21:50:08 +0300

    Display photos in our front-end main page(homepage)

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+<?php require_once("admin/includes/init.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,6 +17,8 @@
 
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
+    
+    
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -23,7 +26,7 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <link rel="stylesheet" href="css/styles.css" />
   </head>
 
   <body>
--- index.php
+++ index.php
@@ -1,15 +1,19 @@
   <?php include("includes/header.php"); ?>
 
-
+  <?php $photos = Photo::find_all(); ?>
   <div class="row">
 
     <!-- Blog Entries Column -->
-    <div class="col-md-8">
-
-    
-
-
-
+    <div class="col-md-12">
+      <div class="thumbnails row">
+        <?php foreach($photos as $photo): ?>
+        <div class="col-xs-6 col-md-3">
+          <a href="" class="thumbnail">
+            <img class="img-responsive home_page_photo" src="admin/<?php echo $photo->picture_path(); ?>" alt="" />
+          </a>
+        </div>
+        <?php endforeach; ?>
+      </div> 
 
     </div>
 
@@ -17,10 +21,10 @@
 
 
     <!-- Blog Sidebar Widgets Column -->
-    <div class="col-md-4">
+    <!--<div class="col-md-4">-->
 
 
-      <?php include("includes/sidebar.php"); ?>
+      <?php // include("includes/sidebar.php"); ?>
 
 
 
