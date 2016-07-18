16ecf606253a923f2f07d5e7bd2c7d78bb8d617e
petrero <petrero@gmail.com>
2016-07-18 22:36:18 +0300

    Copy code from previous branch.

---
+++ index.php
@@ -0,0 +1,34 @@
+  <?php include("includes/header.php"); ?>
+
+  <?php $photos = Photo::find_all(); ?>
+  <div class="row">
+
+    <!-- Blog Entries Column -->
+    <div class="col-md-12">
+      <div class="thumbnails row">
+        <?php foreach($photos as $photo): ?>
+        <div class="col-xs-6 col-md-3">
+          <a href="photo.php?id=<?php echo $photo->id; ?>" class="thumbnail">
+            <img class="img-responsive home_page_photo" src="admin/<?php echo $photo->picture_path(); ?>" alt="" />
+          </a>
+        </div>
+        <?php endforeach; ?>
+      </div> 
+
+    </div>
+
+
+
+
+    <!-- Blog Sidebar Widgets Column -->
+    <!--<div class="col-md-4">-->
+
+
+      <?php // include("includes/sidebar.php"); ?>
+
+
+
+  </div>
+  <!-- /.row -->
+
+  <?php include("includes/footer.php"); ?>
