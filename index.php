b640d43a42f457b6b3c5802edf9c92a68359db88
petrero <petrero@gmail.com>
2016-07-19 19:41:55 +0300

    Create previous and next page link.

--- index.php
+++ index.php
@@ -24,7 +24,20 @@
         </div>
         <?php endforeach; ?>
       </div> 
-
+      <div class="row">
+        <ul class="pager">
+          <?php 
+            if($paginate->page_total()>1){
+              if($paginate->has_next()){
+                echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+              }
+              if($paginate->has_previous()){
+                echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+              }
+            }
+          ?>       
+        </ul>
+      </div>  
     </div>
 
 
