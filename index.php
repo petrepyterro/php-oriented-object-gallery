f3dafd5e5d180ec9b1366bafbcae59d70882f8cb
petrero <petrero@gmail.com>
2016-07-19 20:11:48 +0300

    Indicate the page where we are and looping through pages.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
   <?php 
     $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
-    $items_per_page = 3;
+    $items_per_page = 4;
     $items_total_count = Photo::count_all();
     $paginate = new Paginate($page, $items_per_page, $items_total_count);
     
@@ -30,6 +30,13 @@
             if($paginate->page_total()>1){
               if($paginate->has_next()){
                 echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+              }
+              for($i=1;$i<=$paginate->page_total();$i++){
+                if($i==$paginate->current_page){
+                  echo "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                } else {
+                  echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                }
               }
               if($paginate->has_previous()){
                 echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
