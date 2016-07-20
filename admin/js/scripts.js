5630a6101de576dd4fda7dd2987bd10f368be557
petrero <petrero@gmail.com>
2016-07-20 21:35:18 +0300

    Enable the Apply Selection button with jQuery.

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,3 +1,11 @@
-tinymce.init({selector: 'textarea'});
+$(document).ready(function(){
+  $(".modal_thumbnails").click(function(){
+    $("#set_user_image").prop("disabled", false);
+  });
+  
+  tinymce.init({selector: 'textarea'});
+});
+
+
 
 
