e4c00ed268961d6cbeaf4af7ae3c43f29cab7fc6
petrero <petrero@gmail.com>
2016-07-20 21:50:18 +0300

    Pulling User id with jQuery.

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
+  var user_href;
+  var user_href_splitted;
+  var user_id;
+  
   $(".modal_thumbnails").click(function(){
     $("#set_user_image").prop("disabled", false);
+    user_href = $("#user-id").prop('href');
+    user_href_splitted = user_href.split("=");
+    user_id = user_href_splitted[user_href_splitted.length - 1];
   });
   
   tinymce.init({selector: 'textarea'});
